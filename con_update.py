1084a977f84dca879f525a90942c7144b93b7863
dhendel <deadeye86>
2017-03-13 08:59:59 -0500

    Moved the initial consol session to con_update.

--- con_update.py
+++ con_update.py
@@ -13,31 +13,48 @@ parser.add_option('--get-tags', action="store", dest="getTags", help="Get tags f
 parser.add_option('-T', action="store", dest="tags", type="string", help="Define list of new tags to "
                                                                          "add to service [REQUIRED]")
 parser.add_option('-U', action="store_true", default=False, dest="update", help="Update specified service with tag")
-parser.add_option('--tag-list', action="store", dest="tags_list", help="Define a list of tags to add to service")
-parser.add_option('--node-services', default=False, dest="list_node_services",
-                  type="string", help="Define tag to add to service")
+parser.add_option('--node-services', action="store_true", default=False, dest="list_node_services",
+                  help="Define tag to add to service")
 parser.add_option('-n', action="store", dest="node", help="Define node for --node-services")
 parser.add_option('-p', action="store", dest="port", default=8500, help="Define port default=8500")
-parser.add_option('-H', action="store", dest="host", default="127.0.0.1", help="Define host default=127.0.0.1")
+parser.add_option('-H', action="store", dest="host", help="Define host default=127.0.0.1")
 
 (opts, args) = parser.parse_args()
 
-if opts.update and (not opts.service or not opts.tags):
-    print
-    "Please define the service or service list with the -S flag and/or the tags to add with -T"
+if not opts.host:
+    print "Please set the host flag (-H)"
     sys.exit()
 else:
+    host = opts.host
+
+port = opts.port
+
+c = consul.Consul(host=host, port=port)
+
+
+if opts.update and (not opts.service or not opts.tags):
+    print "Please define the service or service list with the -S flag and/or the tags to add with -T"
+    sys.exit()
+elif opts.update and opts.service and opts.tags:
     services = opts.service.split(',')
     try:
         for i in range(0, len(services)):
-            node = get_service_node(services[i])
-            new_tags = gen_new_tags(services[i], opts.tags)
-            update_tag(node, services[i], new_tags, port=opts.port)
+
+            new_tags = gen_new_tags(c, services[i], opts.tags)
+            update_tag(c, services[i], new_tags, port=opts.port)
 
     except Exception as e:
-        print
-        "Update failed: {0}".format(e)
+        print "Update failed: {0}".format(e)
         sys.exit()
+else:
+    pass
 
 if opts.list_services:
-    list_services()
+    list_services(c)
+
+if opts.list_node_services and not opts.node:
+    print "Please specify a node to get a list of services"
+    sys.exit()
+elif opts.list_node_services:
+    node_services = get_node_services(opts.node, opts.port)
+    print node_services
--- consulUpdate/__init__.py
+++ consulUpdate/__init__.py
@@ -1,10 +1,9 @@
 import consul
 
-c = consul.Consul(port=8500)
 
 
-def get_nodes():
-    nodes_dict = c.catalog.nodes()
+def get_nodes(session):
+    nodes_dict = session.catalog.nodes()
     nodes_list = []
 
     # Get all of the Nodes from the nodes_dict
@@ -27,41 +26,37 @@ def get_node_services(node, port=8500):
     return services_tags
 
 
-def get_service_node(service):
-    services = c.health.service(service)
+def get_service_node(session, service):
+    services = session.health.service(service)
     return services[1][0]['Node']['Node']
 
 
-def get_current_tags(service):
-    tags = c.catalog.service(service)
+def get_current_tags(session, service):
+    tags = session.catalog.service(service)
     return tags[1][0]['ServiceTags']
 
 
-def update_tag(node, service, tag_list, port=8500):
+def update_tag(session, service, tag_list, port=8500):
+    node = get_service_node(session, service)
     n = consul.Consul(host=node, port=port)
 
     try:
         if n.agent.service.register(name=service, tags=tag_list):
-            print
-            "Service {0} has been updated with tags {1} on node {2}".format(service, tag_list, node)
+            print "Service {0} has been updated with tags {1} on node {2}".format(service, tag_list, node)
         else:
-            print
-            "The tag update on service {0} has failed.".format(service)
+            print "The tag update on service {0} has failed.".format(service)
     except Exception as e:
-        print
-        "An error has occurred with the request: {0}".format(e)
+        print "An error has occurred with the request: {0}".format(e)
 
 
-def list_services():
-    print
-    "Available services: "
-    for k in c.agent.services().iteritems():
-        print
-        k[0]
+def list_services(session):
+    print "Available services: "
+    for k in session.agent.services().iteritems():
+        print k[0]
 
 
-def gen_new_tags(service, tags):
-    current_tags = get_current_tags(service)
+def gen_new_tags(session, service, tags):
+    current_tags = get_current_tags(session, service)
 
     new_tags = tags.split(',')
     for i in range(0, len(new_tags)):
