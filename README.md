71b20a1e77ae68862e930e425c08b2e3ba5b9a9b
dhendel <deadeye86>
2017-03-14 09:19:51 -0500

    Moved all code to con_update.py. Added main and a filtered update option.

--- README.md
+++ README.md
@@ -10,9 +10,42 @@ cd consulTagUpdater
 python setup.py install
 ```
 #### Run directly
+
+Note: If you do not have CONSUL_HTTP_ADDR set you must set the host flag -H.
+
+To update specific services:
 ```
-cd consulTagUpdater
-python con_update.py -U -S service1,service2 -T tag1,tag2
+$ python2 con_update.py -U -S service1,service2 -T tag1,tag2
+```
+To list all services with a specific flag:
+```
+$ python2 con_update.py -H 127.0.0.1 -f ENA
+{u'test': [u'ENA'], u'consul': [u'ENA'], u'tag': [u'ENA'], u'adder': [u'ENA']}
+```
+To list all services:
+```
+$ python2 con_update.py -H 127.0.0.1 --ls
+Available services:
+{u'test': [u'rock', u'ENA', u'Tuseday', u'Afternoon', u'proxy-standard', u'TestTag', u'preds'],
+u'consul': [u'preds', u'rock', u'ENA', u'Tuseday', u'Afternoon', u'proxy-standard', u'TestTag'],
+u'tag': [u'Afternoon', u'proxy-standard', u'TestTag', u'preds', u'rock', u'ENA', u'Tuseday'],
+u'adder': [u'proxy-standard', u'TestTag', u'preds', u'rock', u'ENA', u'Tuseday', u'Afternoon', u'SingleService']}
 ```
 
-If you do not have CONSUL_HTTP_ADDR set you must set the host flag -H.
+Update services with a specific tag
+```
+$ python2 con_update.py -H 127.0.0.1 -f ENA -U -T proxy-standard
+Service test has been updated with tags [u'TestTag', u'preds', u'rock', u'ENA', u'Tuseday', u'Afternoon', 'proxy-standard'] on node node0
+Service consul has been updated with tags [u'TestTag', u'preds', u'rock', u'ENA', u'Tuseday', u'Afternoon', 'proxy-standard'] on node node0
+Service tag has been updated with tags [u'TestTag', u'preds', u'rock', u'ENA', u'Tuseday', u'Afternoon', 'proxy-standard'] on node node0
+Service adder has been updated with tags [u'TestTag', u'preds', u'rock', u'ENA', u'Tuseday', u'Afternoon', u'SingleService', 'proxy-standard'] on node node0
+```
+
+Update service tags with a prefix and service name based on filter
+```
+$ python2 con_update.py -H 127.0.0.1 -f ENA -U --prefix proxy- --Rs
+Service test has been updated with tags [u'ENA', u'proxy-test'] on node node0
+Service consul has been updated with tags [u'ENA', u'proxy-consul'] on node node0
+Service tag has been updated with tags [u'ENA', u'proxy-tag'] on node node0
+Service adder has been updated with tags [u'ENA', u'proxy-adder'] on node node0
+```
--- con_update.py
+++ con_update.py
@@ -10,7 +10,7 @@ parser.add_option('-k', action="store", dest="api_key", help="Set API key")
 parser.add_option('--ls', action="store_true", dest="list_services", help="List available services")
 parser.add_option('-S', action="store", dest="service", help="Define which services to edit, "
                                                              "comma separated list \n"
-                                                             "service1,service2,service3 [REQUIRED]")
+                                                             "service1,service2,service3")
 parser.add_option('-f', action="store", dest="filter", help="Filter services for tag updates")
 parser.add_option('--get-tags', action="store", dest="getTags", help="Get tags for service")
 parser.add_option('-T', action="store", dest="tags", type="string", help="Define list of new tags to "
@@ -19,6 +19,10 @@ parser.add_option('-U', action="store_true", default=False, dest="update", help=
 parser.add_option('--node-services', action="store_true", default=False, dest="list_node_services",
                   help="Define tag to add to service")
 parser.add_option('-n', action="store", dest="node", help="Define node for --node-services")
+parser.add_option('--prefix', action="store", dest="prefix", help="The prefix to update a tag with. used with -Rs")
+parser.add_option('--Rs', action="store_true", dest="regex", help="Combine the prefix and the service name to create a new"
+                                                            "tag. ex. -S test --prefix proxy- -Rs would create a tag"
+                                                            "proxy-test.")
 
 
 (opts, args) = parser.parse_args()
@@ -77,7 +81,7 @@ def update_tag(service, tag_list, port=8500, api_key=''):
 
 def list_services():
     print "Available services: "
-    print c.catalog.services()
+    print c.catalog.services()[1]
 
 
 # Use the tags from -T and combine with current tags
@@ -105,19 +109,39 @@ def filtered_update(pattern):
 
 
 def main():
-    if opts.update and (not opts.service or not opts.tags):
-        print "Please define the service or service list with the -S flag and/or the tags to add with -T"
-        sys.exit()
-    elif opts.update and opts.service and opts.tags:
+
+    # if opts.update and (not opts.tags or not opts.filter):
+    #     print "Please define the service or service list with the -S flag and/or the tags to add with -T"
+    #     sys.exit(1)
+
+    if opts.filter and opts.regex:
+        if not opts.prefix:
+            print "Please define the prefix --prefix"
+            sys.exit(1)
+        else:
+            f_services = filtered_update(opts.filter)
+            for k, v in f_services.iteritems():
+                v.append(opts.prefix + k)
+                update_tag(k, v)
+
+    if opts.update and (opts.tags and opts.filter):
+        f_services = filtered_update(opts.filter)
+        new_tags = opts.tags.split(',')
+        for k, v in f_services.iteritems():
+            tags = get_current_tags(k)
+            for i in new_tags:
+                tags.append(i)
+                update_tag(k, tags)
+
+    elif opts.update and (opts.service and opts.tags):
         services = opts.service.split(',')
         try:
             for i in range(0, len(services)):
                 new_tags = gen_new_tags(services[i])
                 update_tag(services[i], new_tags, port=opts.port)
-
         except Exception as e:
             print "Update failed: {0}".format(e)
-            sys.exit()
+            sys.exit(1)
     else:
         pass
 
@@ -126,19 +150,19 @@ def main():
 
     if opts.list_node_services and not opts.node:
         print "Please specify a node to get a list of services"
-        sys.exit()
+        sys.exit(1)
     elif opts.list_node_services:
         node_services = get_node_services(opts.node, opts.port)
         print node_services
 
-    if opts.filter:
+    if opts.filter and not opts.update:
         f_services = filtered_update(opts.filter)
-        new_tags = opts.tags.split(',')
-        for k, v in f_services.iteritems():
-            tags = get_current_tags(k)
-            for i in new_tags:
-                tags.append(i)
-                update_tag(k, tags)
+        print f_services
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
