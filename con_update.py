59de39941a96fc4b24a8c2bb5e16800bcf11324e
dhendel <deadeye86>
2017-03-14 08:44:02 -0500

    Moved all code to con_update.py. Added main and a filtered update option.

--- con_update.py
+++ con_update.py
@@ -1,8 +1,7 @@
 import optparse
-import os
 import sys
 
-from consulUpdate import *
+import consul
 
 parser = optparse.OptionParser()
 parser.add_option('-p', action="store", dest="port", default=8500, help="Define port default=8500")
@@ -12,7 +11,7 @@ parser.add_option('--ls', action="store_true", dest="list_services", help="List 
 parser.add_option('-S', action="store", dest="service", help="Define which services to edit, "
                                                              "comma separated list \n"
                                                              "service1,service2,service3 [REQUIRED]")
-
+parser.add_option('-f', action="store", dest="filter", help="Filter services for tag updates")
 parser.add_option('--get-tags', action="store", dest="getTags", help="Get tags for service")
 parser.add_option('-T', action="store", dest="tags", type="string", help="Define list of new tags to "
                                                                          "add to service [REQUIRED]")
@@ -21,45 +20,125 @@ parser.add_option('--node-services', action="store_true", default=False, dest="l
                   help="Define tag to add to service")
 parser.add_option('-n', action="store", dest="node", help="Define node for --node-services")
 
-(opts, args) = parser.parse_args()
 
-if not opts.host:
-    if os.environ['CONSUL_HTTP_ADDR']:
-        host = os.environ['CONSUL_HTTP_ADDR']
-    else:
-        print "Please set the host flag (-H) or set the CONSUL_HTTP_ADDR env variable"
-        sys.exit()
-else:
-    host = opts.host
+(opts, args) = parser.parse_args()
 
 port = opts.port
 
-c = consul.Consul(host=host, port=port,token=opts.api_key)
+c = consul.Consul(host=opts.host, port=port,token=opts.api_key)
 
 
-if opts.update and (not opts.service or not opts.tags):
-    print "Please define the service or service list with the -S flag and/or the tags to add with -T"
-    sys.exit()
-elif opts.update and opts.service and opts.tags:
-    services = opts.service.split(',')
+def get_nodes():
+    nodes_dict = c.catalog.nodes()
+    nodes_list = []
+
+    # Get all of the Nodes from the nodes_dict
+    for i in range(0, len(nodes_dict[1])):
+        node = nodes_dict[1][i]['Node']
+        nodes_list.append(node)
+
+    return nodes_list
+
+
+# Get all services on a specific node
+def get_node_services(service, node, port=8500, api_key=''):
+    node = consul.Consul(host=node, port=port, token=api_key)
+    node_services = node.agent.services()
+
+    services_tags = {}
+    for k, v in node_services.iteritems():
+        services_tags[k] = v['Tags']
+
+    return services_tags
+
+
+def get_service_node(service):
+    services = c.health.service(service)
+    return services[1][0]['Node']['Node']
+
+
+def get_current_tags(service):
+    tags = c.catalog.service(service)
+    return tags[1][0]['ServiceTags']
+
+
+def update_tag(service, tag_list, port=8500, api_key=''):
+    node = get_service_node(service)
+    n = consul.Consul(host=node, port=port, token=api_key)
+
     try:
-        for i in range(0, len(services)):
-
-            new_tags = gen_new_tags(c, services[i], opts.tags)
-            update_tag(c, services[i], new_tags, port=opts.port)
-
+        if n.agent.service.register(name=service, tags=tag_list):
+            print "Service {0} has been updated with tags {1} on node {2}".format(service, tag_list, node)
+        else:
+            print "The tag update on service {0} has failed.".format(service)
     except Exception as e:
-        print "Update failed: {0}".format(e)
+        print "An error has occurred with the request: {0}".format(e)
+
+
+def list_services():
+    print "Available services: "
+    print c.catalog.services()
+
+
+# Use the tags from -T and combine with current tags
+def gen_new_tags(service):
+    current_tags = get_current_tags(service)
+
+    tags = opts.tags.split(',')
+    for i in range(0, len(tags)):
+        current_tags.append(tags[i])
+
+    return current_tags
+
+
+# Find all services that match the pass string to filter on.
+def filtered_update(pattern):
+    services = c.catalog.services()
+    filtered_services = dict()
+
+    for k, v in services[1].iteritems():
+        matches = [s for s in v if pattern in s]
+        if matches:
+            filtered_services[k] = matches
+
+    return filtered_services
+
+
+def main():
+    if opts.update and (not opts.service or not opts.tags):
+        print "Please define the service or service list with the -S flag and/or the tags to add with -T"
         sys.exit()
-else:
-    pass
+    elif opts.update and opts.service and opts.tags:
+        services = opts.service.split(',')
+        try:
+            for i in range(0, len(services)):
+                new_tags = gen_new_tags(services[i])
+                update_tag(services[i], new_tags, port=opts.port)
 
-if opts.list_services:
-    list_services(c)
+        except Exception as e:
+            print "Update failed: {0}".format(e)
+            sys.exit()
+    else:
+        pass
 
-if opts.list_node_services and not opts.node:
-    print "Please specify a node to get a list of services"
-    sys.exit()
-elif opts.list_node_services:
-    node_services = get_node_services(opts.node, opts.port)
-    print node_services
+    if opts.list_services:
+        list_services()
+
+    if opts.list_node_services and not opts.node:
+        print "Please specify a node to get a list of services"
+        sys.exit()
+    elif opts.list_node_services:
+        node_services = get_node_services(opts.node, opts.port)
+        print node_services
+
+    if opts.filter:
+        f_services = filtered_update(opts.filter)
+        new_tags = opts.tags.split(',')
+        for k, v in f_services.iteritems():
+            tags = get_current_tags(k)
+            for i in new_tags:
+                tags.append(i)
+                update_tag(k, tags)
+
+if __name__ == "__main__":
+    main()
